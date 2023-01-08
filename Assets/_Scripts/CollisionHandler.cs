8af6ab4cc623e4636418557338728d9d14b30c0d
ukg <ukg@shoutrlabs.com>
2023-01-08 18:30:16 +0100

    Added SceneManager, particles, sounds for success and crush

--- Assets/_Scripts/CollisionHandler.cs
+++ Assets/_Scripts/CollisionHandler.cs
@@ -1,14 +1,42 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
     private const string FriendlyTag = "Friendly";
     private const string FinishPadTag = "FinishPad";
-    private const string FuelTag = "Fuel";
 
+    [SerializeField]
+    private float _loadLevelDelay = 1f;
+
+    [SerializeField]
+    private AudioClip _success;
+
+    [SerializeField]
+    private AudioClip _crash;
+
+    [SerializeField]
+    private ParticleSystem _successParticles;
+
+    [SerializeField]
+    private ParticleSystem _crashParticles;
+
+    private AudioSource _audioSource;
+
+    private bool _isTransitioning = false;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
+        if (_isTransitioning)
+        {
+            return;
+        }
+
         switch (collision.gameObject.tag)
         {
             case FriendlyTag:
@@ -16,13 +44,56 @@ public class CollisionHandler : MonoBehaviour
                 break;
             case FinishPadTag:
                 Debug.LogFormat("Congrats, you finished!");
-                break;
-            case FuelTag:
-                Debug.LogFormat("You picked up a {0}", FuelTag);
+                StartSuccessSequence();
                 break;
             default:
                 Debug.LogFormat("Sorry you blew up");
+                StartCrashSequence();
                 break;
         }
+    }
+
+    private void StartSuccessSequence()
+    {
+        _isTransitioning = true;
+
+        _audioSource.Stop();
+        _audioSource.PlayOneShot(_success);
+
+        _successParticles.Play();
+
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", _loadLevelDelay);
+    }
+
+    private void StartCrashSequence()
+    {
+        _isTransitioning = true;
+
+        _audioSource.Stop();
+        _audioSource.PlayOneShot(_crash);
+
+        _crashParticles.Play();
+
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", _loadLevelDelay);
+    }
+
+    private void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+
+    private void LoadNextLevel()
+    {
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+
     }
 }
