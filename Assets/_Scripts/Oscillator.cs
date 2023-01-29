a7b3741fc66fafbf1ec2cf77565a27c9258c5c4a
ukg <ukg@shoutrlabs.com>
2023-01-29 20:33:06 +0100

    Created Oscillator.cs

---
+++ Assets/_Scripts/Oscillator.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _movementVector;
+
+    [SerializeField]
+    private float period = 2f;
+
+    private float _movementFactor;
+
+    private Vector3 _startingPosition;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _startingPosition = transform.position;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float cycles = Time.time / period; // continually growing over time
+
+        const float tau = Mathf.PI * 2; // constant value of 6.283
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1
+
+        _movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1 so its cleaner
+
+        Vector3 offset = _movementVector * _movementFactor;
+        transform.position = _startingPosition + offset;
+    }
+}
